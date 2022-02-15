ca6902fad1fc51e8995a32c66cc2cf9cc71735c4
AndrewH1188 <andrewh1188@gmail.com>
2022-02-15 16:30:55 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KT7vsGkktX7aUBuRhVSV3sOMa3RAiRNcH83eVzlhIzS2ypeb4vwDlc0ocHEwDHy5a7XTXs6s63sQxhUt5QNW2xO00CbRJXLGU',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
